32442ae7f0b09cea8343e5b66d516b6dd6f950ea
Yusuf <yusufhameed911@gmail.com>
2026-07-21 16:25:14 +0530

    Added Groq Ai provider for routerone apis.

---
+++ apps/ai-backend/src/llms/Groq.ts
@@ -0,0 +1,33 @@
+import { Messages } from "../types"
+import { BaseLlm, LlmResponse } from "./Base"
+import Groq from "groq-sdk"
+
+const groq = new Groq({
+    apiKey: process.env.GROQ_API_KEY!,
+})
+
+export class GroqAI extends BaseLlm {
+    static async chat(model: string, messages: Messages): Promise<LlmResponse> {
+        const response = await groq.chat.completions.create({
+            model,
+            messages: messages.map((message) => ({
+                role: message.role,
+                content: message.content,
+            })),
+        })
+
+        return {
+            inputTokensConsumed: response.usage?.prompt_tokens ?? 0,
+            outputTokensConsumed: response.usage?.completion_tokens ?? 0,
+            completions: {
+                choices: [
+                    {
+                        message: {
+                            content: response.choices?.[0]?.message?.content ?? "",
+                        },
+                    },
+                ],
+            },
+        }
+    }
+}
