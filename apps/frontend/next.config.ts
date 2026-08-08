2a60b3ff883640065b972eee4ff2b5afb6b75f5a
Yusuf <yusufhameed911@gmail.com>
2026-08-08 18:28:53 +0530

    Fixed: Nextjs configuration elsia auth cookie.

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,9 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  // Allows importing types (and only types) from the backend workspace package.
-  transpilePackages: [],
-};
+    reactStrictMode: true,
 
-export default nextConfig;
+    // Allows importing types (and only types) from the backend workspace package.
+    transpilePackages: [],
+
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "https://routeronebackend.vercel.app/:path*",
+            },
+        ]
+    },
+}
+
+export default nextConfig
